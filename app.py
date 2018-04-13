667e3ae2b39ce35385d8c92aa54cd957c31302b8
Monica <monicaarbeit@gmail.com>
2018-04-13 15:51:16 -0400

    added db

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+# flask libraries
 from flask import Flask, render_template, url_for, redirect, session, request
 from flask_bootstrap import Bootstrap
+
+#system libraries
+import os
+
+# database related libraries 
+import psycopg2
+import urllib.parse
+import uuid
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -18,6 +27,19 @@ def login():
 @app.route('/message', methods=['GET', 'POST'])
 def message():
 	return render_template("message.html")
+
+# database
+urllib.parse.uses_netloc.append("postgres")
+url = urllib.parse.urlparse(os.environ['DATABASE_URL'])
+
+conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+cur = conn.cursor()
 
 if __name__ == "__main__":
 	app.run()
